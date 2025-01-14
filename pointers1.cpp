f627f62c52f065dee42b47025dc7d74a91908e7b
Anurag Bhonsle <112650799+anuragbhonsle@users.noreply.github.com>
2025-01-14 23:30:10 +0530

    Add files via upload

---
+++ pointers1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*
+int num = 5;
+//cout << "This is the number before we assign a pointer " << num << endl;
+int *p = 0; // assigining a pointer to null
+p = &num;   // assigining a pointer to the address of num
+(*p)++; // increment the value of num through a pointer 
+int *q = p;
+//cout << "*p " << *p << endl;
+//cout << "*q " << *q << endl;
+
+int i =3;
+int *t = &i;
+
+cout << ++*t << endl;
+
+cout << endl;
+
+//cout << t << endl;
+
+*t = *t + 1000000;
+
+cout << *t << endl;
+
+
+int arr[3] = {5, 7, 2};
+int *ptr = arr;
+for (int i = 0; i < 3; i++) {
+   cout << *(ptr + i) << " ";
+}
+cout << endl;
+cout << arr <<endl;
+cout << ptr <<endl;
+cout << *ptr << endl;
+cout << *ptr + 1 <<endl;
+cout << *ptr + 2 <<endl;
+
+cout << endl;
+int arr[5] = {2,4,5,7,8};
+
+int *ptr = &arr[0];
+cout << *ptr << endl;
+cout << ptr << endl;
+cout << &ptr << endl;
+
+cout << endl;
+
+ptr++;
+cout << *ptr << endl;
+cout << ptr << endl;
+cout << &ptr << endl;
+
+cout << endl;
+
+ptr++;
+arr[2] = 24;
+cout << *ptr << endl;
+cout << ptr << endl;
+cout << &ptr << endl;
+*/
+//char ch[5] = "abcd";
+//char *ptr = &ch[0];
+//cout << ptr << endl;
+
+//cout <<endl;
+
+
+
+    char ok = 'z';
+    
+    cout << &ok;
+    
+    return 0;
+
+}
