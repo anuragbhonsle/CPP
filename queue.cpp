57753986eda759d7a37d368064fffe6ad05f4c37
Anurag Bhonsle <112650799+anuragbhonsle@users.noreply.github.com>
2025-02-08 18:11:36 +0530

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+class queue{
+    public:
+    int *arr;
+    int size;
+    int front;
+    int rare;
+    int data;
+
+    queue(int data){
+        this -> data = data;
+
+    }
+};
+int main(){
+
+}
